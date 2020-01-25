ce8cb3713d92657bf470e2c270541840fa5abc0a
Brais Antela <braantela@gmail.com>
2020-01-25 11:53:12 +0100

    Minor changes.

---
+++ index.js
@@ -0,0 +1,5 @@
+import Button from "./src/components/Button"
+import Card from "./src/components/Card"
+import Header from "./src/components/Header"
+
+export { Button, Card, Header }
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import Button from './components/Button'
-import Card from './components/Card'
-import Header from './components/Header'
-
-export { Button, Card, Header }
