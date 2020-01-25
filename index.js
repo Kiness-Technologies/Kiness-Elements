2391743624d8a1c76c205da9400c4783918ab471
Brais Antela <braantela@gmail.com>
2020-01-25 18:26:12 +0100

    Added ListItem properly

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Button from "./src/components/Button"
 import Card from "./src/components/Card"
 import Header from "./src/components/Header"
+import StyledListItem as ListItem from "./src/components/ListItem"
 
-export { Button, Card, Header }
+export { Button, Card, Header, ListItem }
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import { ListItem as Precursor } from 'react-native-elements'
-import styled from 'styled-components'
+import React from "react"
+import { ListItem as Precursor } from "react-native-elements"
+import styled from "styled-components"
 
 function ListItem(props) {
   return <Precursor {...props}>{props.children}</Precursor>
 }
 
 function changeStyle(props) {
-  if (props.card) {
-    return {
-      borderRadius: 7,
-      borderColor: 'transparent',
-      shadowColor: 'rgba(0,0,0,0.2)',
-      shadowOffset: { height: 3, width: 0 },
-      shadowOpacity: 0.7,
-      shadowRadius: 5,
-      margin: 15,
-      padding: 15,
-      marginBottom: 0
+  const cardStyle = {
+    borderRadius: 7,
+    borderColor: "transparent",
+    shadowColor: "rgba(0,0,0,0.2)",
+    shadowOffset: { height: 3, width: 0 },
+    shadowOpacity: 0.7,
+    shadowRadius: 5,
+    margin: 15,
+    padding: 15,
+    marginBottom: 0
+  }
+
+  if (props.kinessStyle && props.card) {
+    const returnedTarget = {
+      ...cardStyle,
+      ...props.kinessStyle
     }
+
+    return returnedTarget
+  }
+
+  if (props.card) {
+    return cardStyle
   }
 }
 
