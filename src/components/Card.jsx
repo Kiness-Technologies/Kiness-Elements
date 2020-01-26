86a5c9a02c5839d89e3d749e033777be6e94b9dc
Brais Antela <braantela@gmail.com>
2020-01-26 22:14:54 +0100

    Minor fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Card as Precursor } from 'react-native-elements'
-import TouchableScale from 'react-native-touchable-scale'
+import React from "react"
+import { Card as Precursor } from "react-native-elements"
+import TouchableScale from "react-native-touchable-scale"
+import styled from "styled-components"
 
 function Card(props) {
   return (
     <TouchableScale activeScale={0.95} friction={6} tension={150} {...props}>
       <Precursor
         wrapperStyle={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between'
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between"
         }}
         containerStyle={{
           borderRadius: 7,
-          borderColor: 'transparent',
-          shadowColor: 'rgba(0,0,0,0.2)',
+          borderColor: "transparent",
+          shadowColor: "rgba(0,0,0,0.2)",
           shadowOffset: { height: 3, width: 0 },
           shadowOpacity: 0.7,
           shadowRadius: 5
@@ -30,8 +31,8 @@ function applyStyle(props) {
   if (props.primary) {
     return {
       borderRadius: 7,
-      borderColor: 'transparent',
-      shadowColor: 'rgba(0,0,0,0.2)',
+      borderColor: "transparent",
+      shadowColor: "rgba(0,0,0,0.2)",
       shadowOffset: { height: 3, width: 0 },
       shadowOpacity: 0.7,
       shadowRadius: 5
