81e11a2cc1ba66fc8458343c98f64cbe6e8f9dfb
Brais Antela <braantela@gmail.com>
2021-04-30 15:12:25 +0200

    Major Bump: v2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Button as ReButton } from "react-native-elements"
 import styled from "styled-components"
 
+// Ancestor
 function PreButton(props) {
   return <ReButton {...props}>{props.children}</ReButton>
 }
@@ -9,13 +10,13 @@ function PreButton(props) {
 function changeStyle(props) {
   if (props.primary) {
     return {
-      backgroundColor: "black"
+      backgroundColor: "black",
     }
   }
 }
 
 const Button = styled(PreButton).attrs((props) => ({
-  buttonStyle: changeStyle(props)
+  buttonStyle: changeStyle(props),
 }))``
 
 export default Button
